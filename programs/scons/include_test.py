6101033f783a1181fc8e11c9df4620e51ef2bb98
Nathan Lovato <nathan@gdquest.com>
2021-07-17 10:49:40 -0600

    Add: test boilerplate for include and link

---
+++ programs/scons/include_test.py
@@ -0,0 +1,37 @@
+"""Tests for the include.py module."""
+
+from include import *
+
+
+def test_anchor_does_not_exist():
+    """Test that an anchor that doesn't exist raises an exception."""
+    pass
+
+
+def test_file_does_not_exist():
+    """Test that a file that doesn't exist raises an exception."""
+    pass
+
+
+def template_missing_file_or_anchor_arguments():
+    """Test that a template that doesn't exist raises an exception."""
+    pass
+
+
+def test_included_anchor_excludes_other_anchors():
+    """Test that an included anchor excludes other anchors."""
+    pass
+
+
+def test_does_find_duplicate_files():
+    """Test that the program finds duplicate files in the project."""
+    pass
+
+
+def test_does_find_duplicate_anchors():
+    """Test that the program finds duplicate anchors in a file."""
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ programs/scons/link_test.py
@@ -0,0 +1,17 @@
+"""Tests for the link.py build preprocessor."""
+from link import *
+
+
+def test_detects_incorrect_template():
+    """Test that the link.py preprocessor detects incorrect template commands."""
+    pass
+
+
+def test_find_linked_file():
+    """Test that the link.py preprocessor can find linked files."""
+    pass
+
+
+def test_missing_arguments():
+    """Test that the link.py preprocessor fails when arguments are missing."""
+    pass
